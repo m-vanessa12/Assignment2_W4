42c903c0d7da01dbef5d77f9f0f9604c95efcc31
Vanessa MUKAMANZI <90476462+m-vanessa12@users.noreply.github.com>
2021-12-18 00:16:24 +0200

    Update rent_cars.py

--- rent_cars.py
+++ rent_cars.py
@@ -1,67 +1,79 @@
-
-car_one = {"car_model": "ford-ecosport", "released_year": "2009", "acquired_year": "2018", "money_made": 13000,
-           "plate_no": "RAA 553V", "rented_times": 0}
-car_two = {"car_model": "toyota", "released_year": "1890", "acquired_year": "2020", "money_made": 13700,
-           "plate_no": "RAE 890R", "rented_times": 0}
-car_three = {"car_model": "volkswagen", "released_year": "1900", "acquired_year": "2015", "money_made": 23000,
-             "plate_no": "RAB 544E", "rented_times": 0}
-car_four = {"car_model": "honda-accord", "released_year": "1700", "acquired_year": "1017", "money_made": 33000,
-            "plate_no": "RAD 112S", "rented_times": 0}
-car_five = {"car_model": "mercedes", "released_year": "1885", "acquired_year": "2020", "money_made": 43000,
-            "plate_no": "RRA 785U", "rented_times": 0}
-
-cars_information = {'car_one': 'ford-ecosport', 'car_two': 'toyota', 'car_three': 'volkswagen',
-                    'car_four': 'honda-accord', 'car_five': 'mercedes'}
-print(cars_information)
-
-print("\n write for add new car\n write remove for removing a car \n write rent for renting a car \n "
-      "profit for calculating the profit ")
+my_cars = {"ford-ecosport": [2009, 2018, 1300, "RAA 553V", 0],
+           "toyota": [1890.2020, 13700, "RAE 890R,0"],
+           "volkswagen": [1900, 2015, 23000, "RAB 544R", 0],
+           "honda-accord": [1700, 1017, 33000, "RAD 112S", 0],
+           "mercedes": [1885, 2020, 43000, "RRA 785U", 0]}
+rented_cars = []
 
 
-def car_business():
-    global cars_information
-    income = 0
-    times = 0
-    choice = input("-->make your choice: ")
-    if choice == 'add':
-        new_car_model = input("model= ")
-        new_released_year = input("released year= ")
-        new_acquired_year = input("acquired year= ")
-        new_money_made = input("money made= ")
-        new_plate_no = input("plate no= ")
-        new_rent = input("rent times= ")
-        print("New car added")  # message to be recieved if the new car added
-        print("The new added car information: ", new_car_model, new_released_year, new_acquired_year,
-              new_money_made, new_plate_no, new_rent)
-
-        new_car = {"new_car": new_car_model, "released_year": new_released_year,
-                   "acquired_year": new_acquired_year, "money_made": new_money_made,
-                   "plate_no": new_plate_no, "rented_times": new_rent}
-
-        cars_information.update({'car_model': new_car_model})
-        print('Updated List of cars: \n', cars_information)
-
-    elif choice == 'remove':
-        car = input('which one: ')
-        cars_information.pop(car)
-        print(car, "Is removed")
-        print("remained cars are: ", cars_information)
+def remove():
+    for item in my_cars.keys():
+        print(item)
+        car_to_remove = input("Enter the name of the car you want to remove")
+        del my_cars[car_to_remove]
 
 
-    elif choice == 'rent':
-        car = input("which one: ")
-        times = times + 1
-        price = input("how much do you like to pay")
-        cars_information[money_made] = +income
-        print(car, "is rented on $", price, " at this time", times)
-        try_again = input('do you want to perfon another task: ')
-        while try_again == 'yes':
-            print(car_business())
+def rent():
+    for item in my_cars.keys():
+        print(item)
+        car_to_rent = input("Enter the car you want to rent")
+        if car_to_rent in my_cars.keys():
+            amount = int(input("Enter the amount you want to rent the car"))
+            print(f"You decided to rent out {car_to_rent},at rwf {amount}, times in business:{my_cars[car_to_rent][4]}")
+            rented_cars.append(my_cars[car_to_rent])
+            del my_cars[car_to_rent]
+
+            break
+        else:
+            print("The car does not exist")
+            break
 
 
-    elif choice == 'profit':
-        car = input("Which one do you rent out?")
-        income = (input("At what price would you like to rent out the car?"))
+def check_profit():
+    for key, value in my_cars.items():
+        # print()
+        print(f"car:{key},amount_made:{value[2]},no of times in business:{value[0]}")
 
 
-car_business()
+def add_cars():
+    new_car_model = input("model= ")
+    new_released_year = int(input("released year= "))
+    new_acquired_year = int(input("acquired year= "))
+    new_money_made = int(input("money made= "))
+    new_plate_no = input("plate no= ")
+    new_rent = input("rent times= ")
+    times_in_business = int(input("The number of times in business"))
+    my_cars[new_car_model] = [new_released_year, new_acquired_year, new_money_made, new_plate_no, times_in_business]
+    print(f"New car added:{new_car_model}, number plate{new_plate_no},")  # message to be recieved if the new car added
+    print("updated list of cars")
+    for key, value in my_cars.items():
+        print(key, value)
+
+
+# add_cars()
+def start():
+    print("Omondi services\n 1.Add car\n2.rent car\n3.check_profit\nremove car")
+    task = input("Enter the task you wnt to perform, 1-4")
+
+    if task == "1":
+        add_cars()
+    elif task == "2":
+        rent()
+    elif task == "3":
+        check_profit()
+    elif task == "4":
+        remove()
+    else:
+        print("try again")
+
+
+def multitasking():
+    c = True
+    while c:
+        start()
+        another = input("Do you want to perform another task y/n")
+        if another == "n":
+            c = False
+
+
+multitasking()
